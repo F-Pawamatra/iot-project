92afd151e238612779b7e3f75866b3c69e269ae6
Fery Adwarsyah Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2022-11-11 08:46:55 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>LED ON/OFF</title>
+</head>
+
+<body>
+<p align="center">
+<font size="8">
+<b>LED 1 = &nbsp <a
+href="https://api.thingspeak.com/update?api_key=XB72C0L156SGN331&field1=1">ON
+</a></b> / <b><a
+href="https://api.thingspeak.com/update?api_key=XB72C0L156SGN331&field1=0">OFF</a>
+</b> <br>
+<b>LED 2 = &nbsp <a href="https://api.thingspeak.com/update?api_key=XB72C0L156SGN331
+&field2=1">ON </a></b> /<b><a
+href="https://api.thingspeak.com/update?api_key=XB72C0L156SGN331&field2=0">OFF</a></b> <br>
+</font>
+</p>
+</body>
+</html>
